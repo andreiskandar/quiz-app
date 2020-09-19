23bfc214c3ae163d866379246feacdf13a9c9bb4
Doug Dyson <dougdyson@gmail.com>
2020-09-19 13:51:34 -0700

    seed quiz_categories table

--- db/schema/01_quiz_categories.sql
+++ db/schema/01_quiz_categories.sql
@@ -4,6 +4,8 @@ DROP TABLE IF EXISTS quiz_categories CASCADE;
 CREATE TABLE quiz_categories (
   id                SERIAL PRIMARY KEY NOT NULL,
   name              VARCHAR(255) NOT NULL,
+  bg_image_url      TEXT,
+  color             varchar(40),
   user_id           VARCHAR(255),
   active            BOOLEAN DEFAULT false
 );
