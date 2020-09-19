f0d5c47fb39ff4cef3060bb69ad9656620ded639
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-19 11:30:09 -0700

    added and modified as a result of merge conflict

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //this file is routing to GET/API/USERS - we don't have an API we can route to?
 // GET /api/users 404 1.418 ms - 148
 // $(() => {
@@ -11,22 +10,3 @@
 //     }
 //   });;
 // });
-=======
-$(() => {
-  // bind button to radio button
-  $(`.option1-btn`).click(() => {
-    console.log("button click");
-    // $(`.option1-btn`);
-    $(`#option1`).attr("checked", "checked");
-  });
-
-  $.ajax({
-    method: "GET",
-    url: "/api/users",
-  }).done((users) => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
-});
->>>>>>> index
