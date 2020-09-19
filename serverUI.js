f2668ed14955242f36c8c323a8295db63fd9cbd8
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-19 05:52:11 -0700

    setup server for UI demo and jQuery

--- serverUI.js
+++ serverUI.js
@@ -3,6 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
+// app.set("view engine", "ejs");
+
+// app.get("/", (req, res) => {
+//   res.render("index", {});
+// });
 app.listen(PORT, () => {
   console.log("Quiz app listening on port: " + PORT);
 });
