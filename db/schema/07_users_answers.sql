a127c3be1f05f7fa2ce3394c064920c514236ba7
Doug Dyson <dougdyson@gmail.com>
2020-09-19 12:18:41 -0700

    rename responses table to users_answers

--- db/schema/07_users_answers.sql
+++ db/schema/07_users_answers.sql
@@ -1,7 +1,7 @@
--- Drop and recreate responses table (Example)
+-- Drop and recreate users_answers table (Example)
 
-DROP TABLE IF EXISTS responses CASCADE;
-CREATE TABLE responses (
+DROP TABLE IF EXISTS users_answers CASCADE;
+CREATE TABLE users_answers (
   id              SERIAL PRIMARY KEY NOT NULL,
   answer_id       INTEGER REFERENCES answers(id),
   user_id         INTEGER REFERENCES users(id),
