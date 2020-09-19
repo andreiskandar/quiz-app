305d9d21c10a97668ce6da3cb1d334a0d5e28c3a
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-19 05:23:22 -0700

    convert buttons using bootstrap

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,17 @@
 $(() => {
+  // bind button to radio button
+  $(`.option1-btn`).click(() => {
+    console.log("button click");
+    // $(`.option1-btn`);
+    $(`#option1`).attr("checked", "checked");
+  });
+
   $.ajax({
     method: "GET",
-    url: "/api/users"
+    url: "/api/users",
   }).done((users) => {
-    for(user of users) {
+    for (user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
 });
