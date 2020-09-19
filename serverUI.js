d72877822f486335cf412c9be0ab3609cd72b908
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-19 06:37:16 -0700

    setup nodemon

--- serverUI.js
+++ serverUI.js
@@ -3,6 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Set public folder as root
 app.use(express.static("public"));
 
 // app.set("view engine", "ejs");
@@ -10,6 +11,7 @@ app.use(express.static("public"));
 // app.get("/", (req, res) => {
 //   res.render("index", {});
 // });
+
 app.listen(PORT, () => {
   console.log("Quiz app listening on port: " + PORT);
 });
