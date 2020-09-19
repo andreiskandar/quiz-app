5dfd957f7c96f920d0840279ee783f6ca5d43a70
Doug Dyson <dougdyson@gmail.com>
2020-09-19 16:01:21 -0700

    test seed quizzes table

---
+++ db/seeds/02_quizzes.sql
@@ -0,0 +1,2 @@
+-- quiz_categories table seeds here
+INSERT INTO quizzes (quiz_category_id, name, url_link, pin, public, time_limit , user_id, active) VALUES (1, 'W4 QUIZ', 'http://cats.com', 123456, true, 600, 47, true);
