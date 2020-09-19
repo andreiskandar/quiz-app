17df3e5b39719be0157e098d21356a944398d2e1
Doug Dyson <dougdyson@gmail.com>
2020-09-19 16:01:21 -0700

    test seed quizzes table

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -3,14 +3,12 @@
 DROP TABLE IF EXISTS quizzes CASCADE;
 CREATE TABLE quizzes (
   id                SERIAL PRIMARY KEY NOT NULL,
-  quiz_category_id  INTEGER REFERENCES quiz_categories(id),
+  quiz_category_id  INTEGER,
   name              VARCHAR(255) NOT NULL,
   url_link          TEXT,
   pin               INTEGER,
   public            BOOLEAN DEFAULT true,
   time_limit        INTEGER,
-  start_time        INTEGER,
-  end_time          INTEGER,
   user_id           VARCHAR(255),
   active            BOOLEAN DEFAULT false
 );
