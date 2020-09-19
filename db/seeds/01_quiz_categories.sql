48f17d83df73486506aee73b8c1c8d5409caa729
Doug Dyson <dougdyson@gmail.com>
2020-09-19 13:37:44 -0700

    seed quiz_categories table

---
+++ db/seeds/01_quiz_categories.sql
@@ -0,0 +1,4 @@
+-- quiz_categories table seeds here
+INSERT INTO quiz_categories (name, user_id, active) VALUES ('Programming', 47, true);
+INSERT INTO quiz_categories (name, user_id, active) VALUES ('Check-In', 47, true);
+INSERT INTO quiz_categories (name, user_id, active) VALUES ('Rating', 47, true);
