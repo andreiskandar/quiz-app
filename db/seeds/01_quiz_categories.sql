9494e186d6c7a46c4786a150a5a2a44c154b16a7
Doug Dyson <dougdyson@gmail.com>
2020-09-19 13:51:34 -0700

    seed quiz_categories table

--- db/seeds/01_quiz_categories.sql
+++ db/seeds/01_quiz_categories.sql
@@ -1,4 +1,4 @@
 -- quiz_categories table seeds here
-INSERT INTO quiz_categories (name, user_id, active) VALUES ('Programming', 47, true);
-INSERT INTO quiz_categories (name, user_id, active) VALUES ('Check-In', 47, true);
-INSERT INTO quiz_categories (name, user_id, active) VALUES ('Rating', 47, true);
+INSERT INTO quiz_categories (name, user_id, bg_image_url, color, active) VALUES ('Programming', 47, 'https://www.transparenttextures.com/patterns/45-degree-fabric-light.png', 'blue', true);
+INSERT INTO quiz_categories (name, user_id, bg_image_url, color, active) VALUES ('Check_In', 47, 'https://www.transparenttextures.com/patterns/arabesque.png', 'green',  true);
+INSERT INTO quiz_categories (name, user_id, bg_image_url, color, active) VALUES ('Rating', 47, 'https://www.transparenttextures.com/patterns/basketball.png', 'orange', true);
