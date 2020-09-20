1fb00a1a1d0d63ca4d167d80fd0d0a9745fba6f7
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-20 16:43:37 -0700

    changed query for quizzes to get a users quizzes by user ID, modified the my-quizzes route and ejs page

--- routes/get-my-quizzes.js
+++ routes/get-my-quizzes.js
@@ -2,25 +2,26 @@ const express = require('express');
 const router = express.Router();
 //we will replace this later
 const { getUsers, getUserById } = require('../db/queries/user-queries');
+const { getQuizzes, getQuizById, getQuizzesByUserId} = require('../db/queries/quiz-queries');
 
 
 // GET /quiz/
 //please replace with quiz-queries
-router.get('/', (req, res) => {
-  getUsers()
-  .then((users) => {
-    res.render('my-quizzes', { users });
-  });
-});
-
-//GET /quiz/:id
-//please replace with quiz-queries
-// router.get('/:id', (req, res) => {
-//   getUserById(req.params.id)
-//   .then((user) => {
-//     res.render('quiz', { user });
+// router.get('/', (req, res) => {
+//   getUsers()
+//   .then((users) => {
+//     res.render('my-quizzes', { users });
 //   });
 // });
+
+//GET /quiz/:id
+router.get('/', (req, res) => {
+  getQuizzesByUserId(req.session.id)
+  .then((quizzes) => {
+    console.log(quizzes)
+    res.render('my-quizzes', { quizzes });
+  });
+});
 
 
 
--- routes/quiz-routes.js
+++ routes/quiz-routes.js
@@ -2,24 +2,25 @@ const express = require('express');
 const router = express.Router();
 //we will replace this later
 const { getUsers, getUserById } = require('../db/queries/user-queries');
+const { getQuizzes, getQuizById, getQuizzesByUserId} = require('../db/queries/quiz-queries');
 
 
 // GET /quiz/ This route is confusing me
 //please replace with quiz-queries
 router.get('/', (req, res) => {
-  getUsers()
-  .then((users) => {
-    res.render('my-quizzes', { users });
+  getQuizzesByUserId(req.params.id)
+  .then((quizzes) => {
+    res.render('my-quizzes', { quizzes });
   });
 });
 
 //GET /quiz/:id
 //please replace with quiz-queries
-router.get('/:id', (req, res) => {
-  getUserById(req.params.id)
-  .then((user) => {
-    res.render('quiz', { user });
-  });
-});
+// router.get('/:id', (req, res) => {
+//   getQuizzesByUserId(id)
+//   .then((quizzes) => {
+//     res.render('quiz', { quiz });
+//   });
+// });
 
 module.exports = router;
