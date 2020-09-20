60e28558e12a8089064635281502ad7d015144c6
Doug Dyson <dougdyson@gmail.com>
2020-09-20 10:52:47 -0700

    seed users_quizzes with 1 user for 1 quiz

--- db/seeds/07_users_quizzes.sql
+++ db/seeds/07_users_quizzes.sql
@@ -1,1 +1,1 @@
-INSERT INTO user_quizzes (quiz_id, user_id, time_start, time_stop, active) VALUES (2, 2, )
+INSERT INTO users_quizzes (quiz_id, user_id, time_start, time_stop, active) VALUES (2, 2, '2020-09-20 17:43:59 +0000', CURRENT_TIMESTAMP, true);
