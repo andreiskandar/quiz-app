d41d7b0076b630d551118afc1e10b1af04d4b6de
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-19 23:06:57 -0700

    fix resetDB now works with a setTimeout and very simple execution and refactoring, fixed a couple of the schema and created a fake seed data for question_categories, will probably be changed and superceded eventually as I was just using it to test resetDB

--- db/schema/03_question_categories.sql
+++ db/schema/03_question_categories.sql
@@ -5,7 +5,7 @@ CREATE TABLE question_categories (
   id        SERIAL PRIMARY KEY NOT NULL,
   name      VARCHAR(255) NOT NULL,
   user_id   INTEGER,
-  active    BOOLEAN DEFAULT false
+  active    BOOLEAN DEFAULT false,
   img_link  TEXT,
   color     VARCHAR(40)
 );
