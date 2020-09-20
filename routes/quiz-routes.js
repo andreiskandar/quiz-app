91fb34c204f5b58e1425cee0d57851ad585d396e
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-19 17:45:19 -0700

    rename product queries to quiz-queries, made some modifications to make user queries work, tided up middleware interactions

--- routes/quiz-routes.js
+++ routes/quiz-routes.js
@@ -3,17 +3,9 @@ const router = express.Router();
 //we will replace this later
 const { getUsers, getUserById } = require('../db/user-queries');
 
-// const { getProductById, getProducts } = require('../db/product-queries');
-// add middleware
-// router.use((req, res, next) => {
-//   // if (!req.session.user_id) {
-//   //   return res.redirect('/login');
-//   // }
-//   console.log('product middleware called');
-//   next();
-// });
-// GET /quiz/
-//these will not be cats once we have quiz data to generate
+
+// GET /quiz/ This route is confusing me
+//please replace with quiz-queries
 router.get('/', (req, res) => {
   getUsers()
   .then((users) => {
@@ -22,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 //GET /quiz/:id
-//these will not be cats once we have quiz data to generate
+//please replace with quiz-queries
 router.get('/:id', (req, res) => {
   getUserById(req.params.id)
   .then((user) => {
