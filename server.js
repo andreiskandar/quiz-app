5c121b0d320f9291a0140d3f84ce78478387c6cd
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-19 18:42:11 -0700

    latest push - housekeeping

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const homeRoutes = require('./routes/get-home-routes');
 const dashboardRoutes = require('./routes/get-dashboard');
 
 const app = express();
-<<<<<<< HEAD
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
@@ -27,51 +26,7 @@ app.use('/dashboard', dashboardRoutes);
 
 // hands this routing off to cat-query-test and that file handles routes
 app.use('/quiz', quizRoutes);
-=======
-const morgan = require("morgan");
 
-// PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
-const db = new Pool(dbParams);
-db.connect();
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan("dev"));
-
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  "/styles",
-  sass({
-    src: __dirname + "/styles",
-    dest: __dirname + "/public/styles",
-    debug: true,
-    outputStyle: "expanded",
-  })
-);
-app.use(express.static("public"));
-
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-// const usersRoutes = require("./routes/users");
-// const widgetsRoutes = require("./routes/widgets");
-
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// app.use("/api/users", usersRoutes(db));
-// app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
->>>>>>> index
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
