709a8cdf29d6f8b815eb18050827ac45ef282c1b
Doug Dyson <dougdyson@gmail.com>
2020-09-20 09:28:41 -0700

    see questions table

---
+++ db/seeds/04_questions.sql
@@ -0,0 +1,4 @@
+INSERT INTO questions (question_category_id, question, time_limit , user_id, active) VALUES (null, 'Which would take priority, an id (#my-id) in a CSS file or inline styling (style="...")?', 0, 47, true);
+INSERT INTO questions (question_category_id, question, time_limit , user_id, active) VALUES (null, 'What colour would render given the hex code #FF0000', 0, 47, true);
+INSERT INTO questions (question_category_id, question, time_limit , user_id, active) VALUES (null, 'Which of the following CSS selector has the highest specificity?', 0, 47, true);
+INSERT INTO questions (question_category_id, question, time_limit , user_id, active) VALUES (null, 'In what order do the following surround an html element according to the Box Model?', 0, 47, true);
