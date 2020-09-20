81e677bf2ec14f3fc997390a6d91c48d33a133ea
Doug Dyson <dougdyson@gmail.com>
2020-09-20 10:12:52 -0700

    schema update from applying real seed data

--- db/schema/04_questions.sql
+++ db/schema/04_questions.sql
@@ -5,7 +5,10 @@ CREATE TABLE questions (
   id                      SERIAL PRIMARY KEY NOT NULL,
   question_category_id    INTEGER,
   question                TEXT NOT NULL,
+  sort_order              INTEGER,
   time_limit              INTEGER,
+  bg_image_url            TEXT,
+  color                   VARCHAR(40),
   user_id                 INTEGER,
   active                  BOOLEAN DEFAULT false
 );
