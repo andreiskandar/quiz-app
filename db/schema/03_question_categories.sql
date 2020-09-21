0ffed6c51bf588fb02f94b095cdaefd547b06117
Doug Dyson <dougdyson@gmail.com>
2020-09-20 18:05:46 -0700

    move active column position, remove comment

--- db/schema/03_question_categories.sql
+++ db/schema/03_question_categories.sql
@@ -5,8 +5,8 @@ CREATE TABLE question_categories (
   id            SERIAL PRIMARY KEY NOT NULL,
   name          VARCHAR(255) NOT NULL,
   user_id       INTEGER,
-  active        BOOLEAN DEFAULT false,
   image_url     TEXT,
   bg_image_url  TEXT,
   color         VARCHAR(40)
+  active        BOOLEAN DEFAULT false,
 );
