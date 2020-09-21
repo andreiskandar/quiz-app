24f2bfb54e2e4d160a3100b5b3cca80d90123a3d
Doug Dyson <dougdyson@gmail.com>
2020-09-20 17:25:47 -0700

    add description and styling columns

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -4,11 +4,15 @@ DROP TABLE IF EXISTS quizzes CASCADE;
 CREATE TABLE quizzes (
   id                SERIAL PRIMARY KEY NOT NULL,
   quiz_category_id  INTEGER,
-  name              VARCHAR(255) NOT NULL,
+  name              VARCHAR(100) NOT NULL,
+  description       VARCHAR(254),
   url_link          TEXT,
   pin               INTEGER,
   public            BOOLEAN DEFAULT true,
   time_limit        INTEGER,
+  image_url         TEXT,
+  bg_image_url      TEXT,
+  color             VARCHAR(40),
   user_id           VARCHAR(255),
   active            BOOLEAN DEFAULT false
 );
