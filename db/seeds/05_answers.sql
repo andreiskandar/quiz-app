2e7fedf55fc603ebb2f11bb07abd9ee9ef819a21
Doug Dyson <dougdyson@gmail.com>
2020-09-20 22:05:22 -0700

    additional seed data

--- db/seeds/05_answers.sql
+++ db/seeds/05_answers.sql
@@ -1,3 +1,12 @@
-INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Depends on what style that it is applying', false, 1, null, null, null, 47, true);
-INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'The id, #my-id would take priority', true, 2, null, null, null, 47, true);
-INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'The inline style', false, 3, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (1, 'Depends on what style that it is applying', false, 1, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (1, 'The id, #my-id would take priority', true, 2, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (1, 'The inline style', false, 3, null, null, null, 47, true);
+
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Red', true, 1, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Blue', false, 2, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Green', false, 3, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Yellow', false, 3, null, null, null, 47, true);
+
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (3, 'li .nav-item { ... }', true, 1, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (3, '#page-header { ... }', false, 2, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (3, 'div p { ... }', false, 3, null, null, null, 47, true);
