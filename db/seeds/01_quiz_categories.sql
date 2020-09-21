ccd751c4eda3fa2f5dcaafcb58abfc36c3f93f60
Doug Dyson <dougdyson@gmail.com>
2020-09-20 17:53:28 -0700

    add image_url

--- db/seeds/01_quiz_categories.sql
+++ db/seeds/01_quiz_categories.sql
@@ -1,5 +1,5 @@
 -- quiz_categories table seeds here
-INSERT INTO quiz_categories (name, user_id, bg_image_url, color, active) VALUES ('WD-W4D1', 47, 'https://www.transparenttextures.com/patterns/45-degree-fabric-light.png', 'blue', true);
-INSERT INTO quiz_categories (name, user_id, bg_image_url, color, active) VALUES ('WD-W4D2', 47, 'https://www.transparenttextures.com/patterns/arabesque.png', 'green',  true);
-INSERT INTO quiz_categories (name, user_id, bg_image_url, color, active) VALUES ('WD-W4D3', 47, 'https://www.transparenttextures.com/patterns/basketball.png', 'orange', true);
-INSERT INTO quiz_categories (name, user_id, bg_image_url, color, active) VALUES ('GRADUATION', 47, 'https://www.transparenttextures.com/patterns/basketball.png', 'gold', true);
+INSERT INTO quiz_categories (name, user_id, image_url, bg_image_url, color, active) VALUES ('WD-W4D1', 47, null, 'https://www.transparenttextures.com/patterns/45-degree-fabric-light.png', 'blue', true);
+INSERT INTO quiz_categories (name, user_id, image_url, bg_image_url, color, active) VALUES ('WD-W4D2', 47, null, 'https://www.transparenttextures.com/patterns/arabesque.png', 'green',  true);
+INSERT INTO quiz_categories (name, user_id, image_url, bg_image_url, color, active) VALUES ('WD-W4D3', 47, null, 'https://www.transparenttextures.com/patterns/basketball.png', 'orange', true);
+INSERT INTO quiz_categories (name, user_id, image_url, bg_image_url, color, active) VALUES ('GRADUATION', 47, null, 'https://www.transparenttextures.com/patterns/basketball.png', 'gold', true);
