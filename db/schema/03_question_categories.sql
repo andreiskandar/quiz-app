e5b8f22a0885dbba76644e3b2ef8d83df1700295
Doug Dyson <dougdyson@gmail.com>
2020-09-20 17:58:38 -0700

    added image_url

--- db/schema/03_question_categories.sql
+++ db/schema/03_question_categories.sql
@@ -2,15 +2,11 @@
 
 DROP TABLE IF EXISTS question_categories CASCADE;
 CREATE TABLE question_categories (
-  id        SERIAL PRIMARY KEY NOT NULL,
-  name      VARCHAR(255) NOT NULL,
-  user_id   INTEGER,
-  active    BOOLEAN DEFAULT false,
-<<<<<<< HEAD
-  img_link  TEXT,
-=======
-  img_link_url  TEXT,
-  bg_image_url TEXT,
->>>>>>> db_setup
-  color     VARCHAR(40)
+  id            SERIAL PRIMARY KEY NOT NULL,
+  name          VARCHAR(255) NOT NULL,
+  user_id       INTEGER,
+  active        BOOLEAN DEFAULT false,
+  image_url     TEXT,
+  bg_image_url  TEXT,
+  color         VARCHAR(40)
 );
