853ee9730bb22e14759b0e849c00af95a43e4c8c
Doug Dyson <dougdyson@gmail.com>
2020-09-20 17:46:54 -0700

    add img_url

--- db/schema/05_answers.sql
+++ db/schema/05_answers.sql
@@ -3,16 +3,12 @@
 DROP TABLE IF EXISTS answers CASCADE;
 CREATE TABLE answers (
   id              SERIAL PRIMARY KEY NOT NULL,
-  question_id     INTEGER;
+  question_id     INTEGER,
   answer          TEXT NOT NULL,
   correct_answer  BOOLEAN,
-<<<<<<< HEAD
-  bg_image        TEXT,
-=======
   sort_order      INTEGER,
-  img_link        TEXT,
+  img_url         TEXT,
   bg_image_url    TEXT,
->>>>>>> 4119bc8e37d69a4ac206d047cc41751f233d9ba2
   color           VARCHAR(40),
   user_id         INTEGER,
   active          BOOLEAN DEFAULT false
