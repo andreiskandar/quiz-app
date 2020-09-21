f959b11a08db10fbc66197a6f344061d9cf62192
Doug Dyson <dougdyson@gmail.com>
2020-09-20 17:46:54 -0700

    add img_url

--- db/seeds/05_answers.sql
+++ db/seeds/05_answers.sql
@@ -1,3 +1,3 @@
-INSERT INTO answers (question_id, answer, correct_answer, sort_order, bg_image_url, color, user_id, active) VALUES (2, 'Depends on what style that it is applying', false, 1, null, null, 47, true);
-INSERT INTO answers (question_id, answer, correct_answer, sort_order, bg_image_url, color, user_id, active) VALUES (2, 'The id, #my-id would take priority', true, 2, null, null, 47, true);
-INSERT INTO answers (question_id, answer, correct_answer, sort_order, bg_image_url, color, user_id, active) VALUES (2, 'The inline style', false, 3, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Depends on what style that it is applying', false, 1, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'The id, #my-id would take priority', true, 2, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'The inline style', false, 3, null, null, null, 47, true);
