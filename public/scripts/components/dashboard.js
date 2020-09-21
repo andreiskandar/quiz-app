eb4efd9172d9e9d45587922448fde6f36525b2c1
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-21 11:00:09 -0700

    added question preview cards on dashboard

--- public/scripts/components/dashboard.js
+++ public/scripts/components/dashboard.js
@@ -1,10 +1,25 @@
 $(() => {
   const $dashboard = $(`  
-    <div class="row">
-      <div class="card col-md">One of three columns</div>
-      <div class="card col-md">One of three columns</div>
-      <div class="card col-md">One of three columns</div>
-    </div>
+  <div class="row">
+  <div class="card col-md">
+    <div class="card quiz_title_dashboard">quiz title</div>
+    <div class="card question_card_dashboard">q1</div>
+    <div class="card question_card_dashboard">q2</div>
+    <div class="card question_card_dashboard">q3</div>
+  </div>
+  <div class="card col-md">
+    <div class="card quiz_title_dashboard">quiz title</div>
+    <div class="card question_card_dashboard">q1</div>
+    <div class="card question_card_dashboard">q2</div>
+    <div class="card question_card_dashboard">q3</div>
+  </div>
+  <div class="card col-md">
+    <div class="card quiz_title_dashboard">quiz title</div>
+    <div class="card question_card_dashboard">q1</div>
+    <div class="card question_card_dashboard">q2</div>
+    <div class="card question_card_dashboard">q3</div>
+  </div>
+</div>
   `);
 
   window.$dashboard = $dashboard;
