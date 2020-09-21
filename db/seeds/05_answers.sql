3644f6f725fc16d17a3c124ac3bd49d884519de8
Doug Dyson <dougdyson@gmail.com>
2020-09-20 22:29:54 -0700

    seed users_answers and answers

--- db/seeds/05_answers.sql
+++ db/seeds/05_answers.sql
@@ -5,8 +5,16 @@ INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, b
 INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Red', true, 1, null, null, null, 47, true);
 INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Blue', false, 2, null, null, null, 47, true);
 INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Green', false, 3, null, null, null, 47, true);
-INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Yellow', false, 3, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (2, 'Yellow', false, 4, null, null, null, 47, true);
 
 INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (3, 'li .nav-item { ... }', true, 1, null, null, null, 47, true);
 INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (3, '#page-header { ... }', false, 2, null, null, null, 47, true);
 INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (3, 'div p { ... }', false, 3, null, null, null, 47, true);
+
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (4, 'Overrides any other declarations, using it is considered bad practice', true, 1, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (4, 'It stands for not important and gets set to a low priority', false, 2, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (4, 'Trump card! Use it when something is really important', false, 3, null, null, null, 47, true);
+
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (5, 'First Padding, Border, then Margin', true, 1, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (5, 'First Padding, Margin, then Border', false, 2, null, null, null, 47, true);
+INSERT INTO answers (question_id, answer, correct_answer, sort_order, img_url, bg_image_url, color, user_id, active) VALUES (5, 'First Border, Margin, then Padding', false, 3, null, null, null, 47, true);
--- db/seeds/08_users_answers.sql
+++ db/seeds/08_users_answers.sql
@@ -1,1 +1,5 @@
-INSERT INTO users_answers (answer_id, user_id, answer_timestamp, active) VALUES (1, 2, '2020-09-20 17:44:59 +0000', true);
+INSERT INTO users_answers (answer_id, user_id, answer_timestamp, active) VALUES (2, 2, '2020-09-20 17:44:59 +0000', true);
+INSERT INTO users_answers (answer_id, user_id, answer_timestamp, active) VALUES (4, 2, '2020-09-20 17:45:59 +0000', true);
+INSERT INTO users_answers (answer_id, user_id, answer_timestamp, active) VALUES (9, 2, '2020-09-20 17:47:59 +0000', true);
+INSERT INTO users_answers (answer_id, user_id, answer_timestamp, active) VALUES (13, 2, '2020-09-20 17:49:59 +0000', true);
+INSERT INTO users_answers (answer_id, user_id, answer_timestamp, active) VALUES (14, 2, '2020-09-20 17:51:48 +0000', true);
