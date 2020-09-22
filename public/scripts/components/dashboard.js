ac6067e8a8f57e40948beb66034379d2911b1414
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-22 13:51:57 -0700

    add ability to do links from dashboard

--- public/scripts/components/dashboard.js
+++ public/scripts/components/dashboard.js
@@ -8,13 +8,17 @@ const getQuizzes = () => {
     for (const quizzes of data) {
 
 
-      randomQuizDomElem += `<div class="card col-md">
+
+
+      randomQuizDomElem +=
+      `
+      <a href="/quizzes/${quizzes.id}">
+      <div class="card col-md">
       <div class="card quiz_title_dashboard">${quizzes.name}</div>
       <div class="card question_card_dashboard">q1</div>
       <div class="card question_card_dashboard">q2</div>
       <div class="card question_card_dashboard">q3</div>
-      </div>`;
-
+      </div></a>`;
     }
     randomQuizDomElem += randomQuizContainerClose;
     window.views_manager.show("quizDashboard", randomQuizDomElem);
@@ -22,38 +26,10 @@ const getQuizzes = () => {
 }
 
 $(() => {
+  //calls getQuizzes so we can have it prepared
 getQuizzes();
-
-  // $.get("/dashboard/my-quizzes", (data) =>{
 
     $("header").on("click", ".brand_btn", () => {
         getQuizzes();
     });
 });
-
-
-// $(() => {
-//   const $dashboard = $(`
-//   <div class="row">
-//   <div class="card col-md">
-//     <div class="card quiz_title_dashboard">quiz title</div>
-//     <div class="card question_card_dashboard">q1</div>
-//     <div class="card question_card_dashboard">q2</div>
-//     <div class="card question_card_dashboard">q3</div>
-//   </div>
-//   <div class="card col-md">
-//     <div class="card quiz_title_dashboard">quiz title</div>
-//     <div class="card question_card_dashboard">q1</div>
-//     <div class="card question_card_dashboard">q2</div>
-//     <div class="card question_card_dashboard">q3</div>
-//   </div>
-//   <div class="card col-md">
-//     <div class="card quiz_title_dashboard">quiz title</div>
-//     <div class="card question_card_dashboard">q1</div>
-//     <div class="card question_card_dashboard">q2</div>
-//     <div class="card question_card_dashboard">q3</div>
-//   </div>
-// </div>
-//   `);
-//   window.$dashboard = $dashboard;
-// });
