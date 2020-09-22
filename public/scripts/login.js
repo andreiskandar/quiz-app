dbbb4af70e8faf2a149ac5f62dfa924567ef5f8a
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-21 21:43:22 -0700

    fixed login issue and logout issue asynchronously

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,9 +1,56 @@
 $(document).ready(() => {
   window.header = {};
+  let currentUser = null;
+
   $("header").hide();
 
   const $loginReg = $(".login_page");
   $(".login_page").append($login);
+
+  const updateHeader = (isTeacher) => {
+    currentUser = isTeacher;
+
+    let userLinks;
+    if (isTeacher) {
+      userLinks = `
+    <header>
+    <ul class="flex-row">
+      <div class="flex-row">
+        <li class="brand brand_btn">Q.a.</li>
+        <li class="browse_btn">Browse</li>
+        <li class="create_btn">Create</li>
+        <li class="myQuiz_btn">My Quizzes</li>
+        <li class="pastQuiz_btn">Past Quizzes</li>
+      </div>
+      <div class="flex-row">
+        <li class="userType_btn">Teacher</li>
+        <li class="logout_btn" id="logout">Logout</li>
+      </div>
+    </ul>
+  </header>
+  `;
+    } else {
+      userLinks = `
+    <header>
+    <ul class="flex-row">
+      <div class="flex-row">
+        <li class="brand brand_btn">Q.a.</li>
+        <li class="browse_btn">Browse</li>
+        <li class="myQuiz_btn">My Quizzes</li>
+        <li class="pastQuiz_btn">Past Quizzes</li>
+      </div>
+      <div class="flex-row">
+        <li class="userType_btn">Student</li>
+        <li class="logout_btn" id="logout">Logout</li>
+      </div>
+    </ul>
+  </header>
+  `;
+    }
+
+    $("#page-header").append(userLinks);
+    views_manager.show("dashboard");
+  };
 
   $loginReg.on("submit", "#login-register", function (e) {
     e.preventDefault();
@@ -11,59 +58,15 @@ $(document).ready(() => {
     //may consider moving these methods to empty()
     $("header").show();
     $("#main-content").show();
-    $.post("/login", { email: email });
-    $(".login_page").hide();
+    $.post("/login", { email }, () => {
+      getUserType().then((user) => {
+        console.log(user);
+        updateHeader(user.is_teacher);
+      });
 
-    const updateHeader = (isTeacher) => {
-      currentUser = isTeacher;
+      $(".login_page").hide();
 
-      let userLinks;
-      if (isTeacher) {
-        userLinks = `
-        <header>
-        <ul class="flex-row">
-          <div class="flex-row">
-            <li class="brand brand_btn">Q.a.</li>
-            <li class="browse_btn">Browse</li>
-            <li class="create_btn">Create</li>
-            <li class="myQuiz_btn">My Quizzes</li>
-            <li class="pastQuiz_btn">Past Quizzes</li>
-          </div>
-          <div class="flex-row">
-            <li class="userType_btn">Teacher</li>
-            <li class="logout_btn" id="logout">Logout</li>
-          </div>
-        </ul>
-      </header>
-      `;
-      } else {
-        userLinks = `
-        <header>
-        <ul class="flex-row">
-          <div class="flex-row">
-            <li class="brand brand_btn">Q.a.</li>
-            <li class="browse_btn">Browse</li>
-            <li class="myQuiz_btn">My Quizzes</li>
-            <li class="pastQuiz_btn">Past Quizzes</li>
-          </div>
-          <div class="flex-row">
-            <li class="userType_btn">Student</li>
-            <li class="logout_btn" id="logout">Logout</li>
-          </div>
-        </ul>
-      </header>
-      `;
-      }
-
-      $("#page-header").append(userLinks);
-      views_manager.show("dashboard");
-      e.preventDefault();
-    };
-
-    window.header.update = updateHeader;
-
-    getUserType().then((user) => {
-      updateHeader(user.is_teacher);
+      window.header.update = updateHeader;
     });
   });
 
@@ -83,17 +86,17 @@ $(document).ready(() => {
   });
 
   $("header").on("click", "#logout", () => {
-    views_manager.show("login");
-    logOut().then(() => {
-      header.update(null);
-    });
+    // logOut().then(() => {
+    //   header.update(null);
+    // });
     //hide the header - do we need to completely destroy these elements for a different user logging in?
     $("header").hide();
-    // $("#main-content").hide();
+    $("#main-content").hide();
     //remove user cookie
-    $.post("/dashboard/logout");
+    $.post("/dashboard/logout", () => {
+      views_manager.show("login");
+    });
     //add the login page
-    console.log("logout form login.js");
     // $(".login_page").show();
     //route to logout to remmove cookie
   });
