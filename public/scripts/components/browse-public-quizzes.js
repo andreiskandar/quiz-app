d279a94a043318a844f0ea5072705b4ce6139571
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-21 19:25:14 -0700

    changed up dynamic script to accomodate a 3 column layout for my-quizzes

--- public/scripts/components/browse-public-quizzes.js
+++ public/scripts/components/browse-public-quizzes.js
@@ -7,10 +7,21 @@ $(() => {
   $("header").on("click", ".browse_btn", () => {
     // $.get("/dashboard/my-quizzes", (data) =>{
       $.get("/dashboard/my-quizzes").then((data) => {
+        let counter = 0;
         $quizDomElem = $publicQuizContainer
         console.log(data)
       for(quizzes of data){
+        if(counter % 3 === 0){
+          $quizDomElem += $publicQuizContainerClose
+          $quizDomElem += $publicQuizContainer
 
+          $quizDomElem += `<div class="card col-md">
+        <div class="card quiz_title_dashboard">${quizzes.name}</div>
+        <div class="card question_card_dashboard">q1</div>
+        <div class="card question_card_dashboard">q2</div>
+        <div class="card question_card_dashboard">q3</div>
+        </div>`
+        } else {
 
         $quizDomElem += `<div class="card col-md">
         <div class="card quiz_title_dashboard">${quizzes.name}</div>
@@ -18,7 +29,8 @@ $(() => {
         <div class="card question_card_dashboard">q2</div>
         <div class="card question_card_dashboard">q3</div>
         </div>`
-
+        }
+        counter++;
       }
       $quizDomElem += $publicQuizContainerClose;
       console.log($quizDomElem);
@@ -28,7 +40,7 @@ $(() => {
 
 window.$quizDomElem = $quizDomElem;
   });
-  window.$quizDomElem = $quizDomElem;
+  // window.$quizDomElem = $quizDomElem;
 });
 
 
