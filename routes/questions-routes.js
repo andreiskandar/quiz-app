e8a65af74fbc302cc079494fafc9935c35aeaa74
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-22 16:13:25 -0700

    render answers option into quiz form

--- routes/questions-routes.js
+++ routes/questions-routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 //we will replace this later
+
+const answerRoutes = require("./answer-routes");
+
 const { getQuestionsFromQuiz } = require("../db/queries/question-queries");
 
 //localhost:3000/quizzes/:quiz_id/questions
@@ -15,6 +18,15 @@ router.get("/:question_id", (req, res) => {
     .catch((e) => console.log("getQuestions from db", e));
 });
 
+//localhost:3000/quizzes/:id/questions/:question_id/answers/
+router.use(
+  "/:question_id/answers",
+  (req, res, next) => {
+    req.question_id = req.params.question_id;
+    next();
+  },
+  answerRoutes
+);
 //localhost:3000/quizzes/:quiz_id/questions/:question_id
 // router.get("/:question_id", (req, res) => {
 //   const quiz_id = req.quiz_id;
