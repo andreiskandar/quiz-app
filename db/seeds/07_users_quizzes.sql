21b40063f686cd5bf1049c7e58f04d9f2b752cb4
Doug Dyson <dougdyson@gmail.com>
2020-09-22 10:36:46 -0700

    add unit tests and seed user 47 as owner of quiz 2

--- db/seeds/07_users_quizzes.sql
+++ db/seeds/07_users_quizzes.sql
@@ -5,3 +5,4 @@ INSERT INTO users_quizzes (quiz_id, user_id, time_start, time_stop, active) VALU
 INSERT INTO users_quizzes (quiz_id, user_id, time_start, time_stop, active) VALUES (2, 7, '2020-09-20 17:13:59 +0000', '2020-09-20 17:19:45 +0000', true);
 INSERT INTO users_quizzes (quiz_id, user_id, time_start, time_stop, active) VALUES (2, 9, '2020-09-20 18:43:59 +0000', '2020-09-20 18:50:45 +0000', false);
 INSERT INTO users_quizzes (quiz_id, user_id, time_start, time_stop, active) VALUES (2, 11, '2020-09-20 18:23:59 +0000', '2020-09-20 18:29:45 +0000', false);
+INSERT INTO users_quizzes (quiz_id, user_id, time_start, time_stop, active) VALUES (2, 47, '2020-09-19 18:23:59 +0000', '2020-09-219 18:29:45 +0000', true);
