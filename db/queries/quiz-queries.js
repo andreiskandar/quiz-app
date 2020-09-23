919f3bfc8179f612b12347204b6dee50f0de0e23
Doug Dyson <dougdyson@gmail.com>
2020-09-23 10:29:23 -0700

    update query

--- db/queries/quiz-queries.js
+++ db/queries/quiz-queries.js
@@ -1,6 +1,6 @@
 const pool = require('../db.js');
 
-//gets all available quizzes
+// gets all available quizzes
 const getQuizzes = (public, active) => {
 
   let published = '';
@@ -27,7 +27,7 @@ const getQuizzes = (public, active) => {
   });
 };
 
-//gets a singular quiz by id
+// gets a singular quiz by quizzes.id
 const getQuizById = (id) => {
   return pool
     .query("SELECT * FROM quizzes WHERE id = $1", [id])
@@ -36,26 +36,71 @@ const getQuizById = (id) => {
     });
 };
 
-//gets all quizzes belonging to that user
-const getQuizzesByUserId = (id) => {
-  return pool
-  .query("SELECT * FROM quizzes WHERE user_id = $1;", [id])
-  .then((response) => {
+// gets all quizzes belonging to that user_ID
+const getQuizzesByUserId = (user_ID) => {
+
+  // add public and active filters
+
+  // build and execute SQL statement
+  const sql = "SELECT * FROM quizzes WHERE user_id = '" + user_ID + "';";
+  return pool.query(sql).then((response) => {
     return response.rows;
-  })
+  });
 }
 
-//gets all quizzes belonging to that user
-const getAllActiveQuizzesData = (id) => {
-  return pool
-  .query("SELECT * FROM quizzes WHERE user_id = $1;", [id])
-  .then((response) => {
+// get questions for quizzes
+const getQuizQuestions = (quiz_id, active) => {
+
+  let activeQuiz = '';
+
+  if (active === 'active') {
+    activeQuiz = " AND questions.active = true;";
+  } else if (activeQuiz === 'inactive') {
+    activeQuiz =  " AND questions.active = false;";
+  }
+
+  // build and execute SQL statement
+  const sql = "SELECT * FROM questions INNER JOIN quizzes ON quizzes.id = questions.quiz_id WHERE questions.quiz_id = " + quiz_id + activeQuiz;
+  return pool.query(sql).then((response) => {
     return response.rows;
-  })
+  });
+}
+
+// get answers for questions from quizzes
+const getAnswersForQuiz = (quiz_id) => {
+  return pool
+    .query("SELECT * FROM answers INNER JOIN questions ON answers.question_id = questions.id INNER JOIN quizzes ON questions.quiz_id = quizzes.id WHERE questions.quiz_id = $1", [id])
+    .then((response) => {
+      return response.rows[0];
+    });
+}
+
+// gets a specified number of random quizzes.ids
+// specified total shouldn't be larger than the
+// available total of public and active quizzes
+const getRandomQuizzes = (total) => {
+
+  const validIDs = [2, 11, 12, 13];
+  const numOfIdsToGet = total;
+  let randomIDs = [];
+
+  // retrieve random ID, add to the return array and
+  // splice the ID from the lookup list to prevent duplication
+  for (let i = 0; i < numOfIdsToGet; i++) {
+    const length = validIDs.length;
+    const randomInt = Math.floor((Math.random() * (length)));
+    const quizID = validIDs[randomInt];
+    randomIDs.push(quizID);
+    validIDs.splice(randomInt, 1);
+  }
+  return randomIDs;
 }
 
 module.exports = {
   getQuizzes,
   getQuizById,
-  getQuizzesByUserId
+  getQuizzesByUserId,
+  getRandomQuizzes,
+  getQuizQuestions,
+  getAnswersForQuiz
 };
