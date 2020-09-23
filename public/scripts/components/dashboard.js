8e0339425899ade494e3dc558d7cd251078d8414
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-22 17:31:42 -0700

    fixing render quiz form issue

--- public/scripts/components/dashboard.js
+++ public/scripts/components/dashboard.js
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 const getQuizzes = () => {
   const randomQuizContainer = `<div class="row random-quizzes">`;
   const randomQuizContainerClose = `</div>`;
 
-  $.get('/quizzes/random').then((data) => {
+  $.get("/quizzes/random").then((data) => {
     // console.log(data);
     let randomQuizDomElem = randomQuizContainer;
     for (const quizzes of data) {
-
-      randomQuizDomElem +=
-      `
+      randomQuizDomElem += `
 
       <div class="card col-md clickable" id="${quizzes.id}">
       <div class="card quiz_title_dashboard">${quizzes.name}</div>
@@ -22,10 +19,8 @@ const getQuizzes = () => {
     randomQuizDomElem += randomQuizContainerClose;
     window.views_manager.show("quizDashboard", randomQuizDomElem);
   });
-}
+};
 
-=======
->>>>>>> interface/quiz-form
 $(() => {
   const $dashboard = $(`  
   <div class="row">
--- public/scripts/components/quiz_form.js
+++ public/scripts/components/quiz_form.js
@@ -1,15 +1,11 @@
 $(() => {
-<<<<<<< HEAD
-=======
   let question_id = 1;
   let quiz_id = 2;
 
   const getQuestionFromDB = () => {
     $.get(`/quizzes/${quiz_id}/questions/${question_id}`).then((questions) => {
       $(".question_number, .question-counter-span").text(questions[0].id);
-      console.log("questions[0].id:", questions[0].id);
       $(".question_string").text(questions[0].question);
-      console.log("questions[0].question:", questions[0].question);
       // $(".question-total-span").text();
     });
   };
@@ -21,110 +17,38 @@ $(() => {
     $.get(`/quizzes/${quiz_id}/questions/${question_id}/answers`).then(
       (answers) => {
         const renderAnswer = answers.map((item, idx) => {
-          $(`.answer${idx + 1}`).text(item.answer);
+          const answerDiv = `
+            <div class="btn btn-outline-light option${question_id}-btn answer-div">
+              <input type="radio" class="radioCustomButton" id="option${question_id}" name="radioGroup" />
+              <label class="answerLabel answer${question_id}">${item.answer}</label>
+            </div>`;
+          $(".answer_form").append(answerDiv);
         });
+        // .answer_form
       }
     );
   };
 
->>>>>>> interface/quiz-form
   const $quizForm = $(`
   <div class="quiz_body">
-  <div class="card question_body">
-    <div>
-<<<<<<< HEAD
-      <h1 class="question_number">1</h1>
-    </div>
-    <div>
-      <h1 class="question_string">
-        What library is used for parsing, validating, manipulating, and
-        formatting dates?
-      </h1>
-    </div>
-    <div class="quiz_body_footer">
+    <div class="card question_body">
       <div>
-        <a href="#" class="back"><i class="fas fa-angle-double-left"></i> </a>
+        <h1 class="question_number"></h1>
       </div>
       <div>
-        <h2 class="question_counter">1/10</h2>
-=======
-      <h1 class="question_number"></h1>
-    </div>
-    <div>
-      <h1 class="question_string"></h1>
-    </div>
-    <div class="quiz_body_footer">
-      <div>
-        <a class="back-btn"><i class="fas fa-angle-double-left"></i> </a>
+        <h1 class="question_string"></h1>
       </div>
-      <div>
-        <h2 class="question_counter"><span class="question-counter-span">1</span>/<span class="question-total-span"></span></h2>
->>>>>>> interface/quiz-form
+      <div class="quiz_body_footer">
+        <div>
+          <a class="back-btn"><i class="fas fa-angle-double-left"></i> </a>
+        </div>
+        <div>
+          <h2 class="question_counter"><span class="question-counter-span">1</span>/<span class="question-total-span"></span></h2>
+        </div>
       </div>
     </div>
-  </div>
-  <form class="rounded answer_form right_bg">
-    <div class="btn btn-outline-light option1-btn answer-div">
-<<<<<<< HEAD
-      <input
-        type="radio"
-        class="radioCustomButton"
-        id="option1"
-        name="radioGroup"
-      />
-      <label class="answerLabel">Answer 1</label>
-    </div>
-    <div class="btn btn-outline-light option2-btn answer-div">
-      <input
-        type="radio"
-        class="radioCustomButton"
-        id="option2"
-        name="radioGroup"
-      />
-      <label class="answerLabel">Answer 2</label>
-    </div>
-    <div class="btn btn-outline-light option3-btn answer-div">
-      <input
-        type="radio"
-        class="radioCustomButton"
-        id="option3"
-        name="radioGroup"
-      />
-      <label class="answerLabel">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, </label>
-    </div>
-    <div class="btn btn-outline-light text-wrap option4-btn answer-div">
-      <input
-        type="radio"
-        class="radioCustomButton"
-        id="option4"
-        name="radioGroup"
-      />
-      <label class="answerLabel">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, ad obcaecati quaerat ex ratione officia fuga quam inventore ipsam placeat</label>
-    </div>
-  </form>
-</div>
-  `);
-
-  window.$quizForm = $quizForm;
-//this covers the whole div
-=======
-      <input type="radio" class="radioCustomButton" id="option1" name="radioGroup" />
-      <label class="answerLabel answer1"></label>
-    </div>
-    <div class="btn btn-outline-light option2-btn answer-div">
-      <input type="radio" class="radioCustomButton" id="option2" name="radioGroup" />
-      <label class="answerLabel answer2">Answer 2</label>
-    </div>
-    <div class="btn btn-outline-light option3-btn answer-div">
-      <input type="radio" class="radioCustomButton" id="option3" name="radioGroup" />
-      <label class="answerLabel answer3"
-        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit,
-      </label>
-    </div>
-    </div>
-    </form>
-    </div>
-    `);
+    <form class="rounded answer_form right_bg"></form>
+  </div> `);
 
   // <div class="btn btn-outline-light text-wrap option4-btn answer-div">
   //   <input type="radio" class="radioCustomButton" id="option4" name="radioGroup" />
@@ -141,34 +65,49 @@ $(() => {
   window.$quizForm = $quizForm;
   const counter = 1;
 
->>>>>>> interface/quiz-form
   $("main").on("click", ".option1-btn", () => {
     $("#option1").prop("checked", true);
-<<<<<<< HEAD
-=======
     // when button is clicks, load next question and answers set
     //get request on the next question with answers set
     //  /quiz/:id/questions/:id
     // /quiz/:id/questions/:id/answers
-    console.log("line 85: answer1 click");
-    $.get(`/quizzes/${quiz_id}/questions/${question_id++}`).then(
+    $(".answer_form").empty();
+    $.get(`/quizzes/${quiz_id}/questions/${++question_id}`).then(
       (questions) => {
-        console.log("line 88 response");
-        $(".question_number").text(questions[0].id);
+        $(".question_number, .question-counter-span").text(questions[0].id);
         $(".question_string").text(questions[0].question);
-        //post request to response table query
-        //keep track right / answer
-        $.get(`/quizzes/${quiz_id}/questions/${question_id++}/answers`).then(
+        // $(".question-total-span").text();
+        $.get(`/quizzes/${quiz_id}/questions/${question_id}/answers`).then(
           (answers) => {
-            const renderAnswer = answers.map((item, idx) =>
-              $(`.answer${idx + 1}`).text(item.answer)
-            );
+            const renderAnswer = answers.map((item, idx) => {
+              console.log("inside render answer");
+              const answerDiv = `
+                <div class="btn btn-outline-light option${question_id}-btn answer-div">
+                  <input type="radio" class="radioCustomButton" id="option${question_id}" name="radioGroup" />
+                  <label class="answerLabel answer${question_id}">${item.answer}</label>
+                </div>`;
+              $(".answer_form").append(answerDiv);
+            });
+            // .answer_form
           }
         );
       }
     );
->>>>>>> interface/quiz-form
   });
+
+  // $.get(`/quizzes/${quiz_id}/questions/${++question_id}`).then(
+  //   (questions) => {
+  //     $(".question_number").text(questions[0].id);
+  //     $(".question_string").text(questions[0].question);
+
+  //     $.get(`/quizzes/${quiz_id}/questions/${question_id}/answers`).then(
+  //       (answers) => {
+  //         console.log("answers:", answers);
+  //         const renderAnswer = answers.map(
+  //           (item, idx) => $(`.answer${idx + 1}`).text(item.answer)
+  //post request to response table query
+  //keep track right / answer
+  // );
 
   $("main").on("click", ".option2-btn", () => {
     $("#option2").prop("checked", true);
@@ -181,24 +120,29 @@ $(() => {
   $("main").on("click", ".option4-btn", () => {
     $("#option4").prop("checked", true);
   });
-<<<<<<< HEAD
-=======
   $("main").on("click", ".back-btn", () => {
     //back to dashboard
     // or back to previous question
->>>>>>> interface/quiz-form
+    $(".answer_form").empty();
 
-    $.get(`/quizzes/${quiz_id}/questions/${question_id--}`).then(
+    $.get(`/quizzes/${quiz_id}/questions/${--question_id}`).then(
       (questions) => {
-        $(".question_number").text(questions[0].id);
+        console.log("question_id:", question_id);
+        $(".question_number, .question-counter-span").text(questions[0].id);
         $(".question_string").text(questions[0].question);
-        //post request to response table query
-        //keep track right / answer
-        $.get(`/quizzes/${quiz_id}/questions/${question_id--}/answers`).then(
+        // $(".question-total-span").text();
+        $.get(`/quizzes/${quiz_id}/questions/${question_id}/answers`).then(
           (answers) => {
-            const renderAnswer = answers.map((item, idx) =>
-              $(`.answer${idx + 1}`).text(item.answer)
-            );
+            const renderAnswer = answers.map((item, idx) => {
+              console.log("object");
+              const answerDiv = `
+                <div class="btn btn-outline-light option${question_id}-btn answer-div">
+                  <input type="radio" class="radioCustomButton" id="option${question_id}" name="radioGroup" />
+                  <label class="answerLabel answer${question_id}">${item.answer}</label>
+                </div>`;
+              $(".answer_form").append(answerDiv);
+            });
+            // .answer_form
           }
         );
       }
