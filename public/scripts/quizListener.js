76d6cabf6e91069660ef7d4cee83c09de57c16ae
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-22 22:48:01 -0700

    using document.on click now outside of document.ready to account for the selector bug we were encountering. I have pathing working correctly now but logic needs to be fixed in quiz_form.js

--- public/scripts/quizListener.js
+++ public/scripts/quizListener.js
@@ -1,13 +1,25 @@
+$(document).on('click', '.clickable', function(e){
+    console.log(e.target)
+  const quizID = e.target.id
+  localStorage.setItem('quiz_id', quizID)
+  console.log(localStorage.getItem('quiz_id'), "in quizListener.js")
+  views_manager.show("quizForm");
+})
+
 $(() => {
 
-$('#main-content').click('.clickable', function(e) {
-  console.log(e.target)
-  window.quiz_id = e.target.id
-  $.get(`/quizzes/${window.quiz_id}/`, () => {
-    console.log("hello");
-    views_manager.show(null, window.$quizForm);
-});
-});
+// $('#main-content').closest()('click', function(){
+//   console.log("I do exist")
+// } )
+
+
+// $('#main-content').click('.clickable', function(e) {
+//   console.log(e.target)
+//   const quizID = e.target.id
+//   localStorage.setItem('quiz_id', quizID)
+//   console.log(localStorage.getItem('quiz_id'), "in quizListener.js")
+//   views_manager.show("quizForm");
+// });
 
 });
 
